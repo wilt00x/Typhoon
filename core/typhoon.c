d99c9eed35400b6775b26395e38d37bbcdea83d6
wilt00x <William@localhost.localdomain>
2019-01-17 17:48:58 +0100

    add main file to access the modules of this tool via a command line

--- core/typhoon.c
+++ core/typhoon.c
@@ -2,18 +2,26 @@
 #include <stdlib.h>
 
 
-/* 
+/*
  * Header's project file.
  */
 #include "../hexedit/src/hexedit.c"
+
+static char *arguments_list[] =
+{
+	"-H",
+	"--hexedit", // equivalent to '-H'
+
+};
 
 int main(int argc, char **argv)
 {
 	if(argc > 1)
 	{
+		printf("tab: %c\n", arguments_list[]);
 		char *buff = save_hexadecimal_format_file((const char*)argv[1]);
 		printf("%s", buff);
-	} else 
+	} else
 	{
 		printf("usage: %s <file_location>.\n", argv[0]);
 		exit(1);
