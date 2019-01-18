9632fc83cb465bb552f4c7867b66862de7e41efa
root <root@localhost.localdomain>
2019-01-19 00:13:41 +0100

    add file containing some essential information and suffixes

---
+++ core/infos.h
@@ -0,0 +1,43 @@
+/*
+-> '/core/infos.h'
+
+Copyright (c) 2019 B.Will.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// DEVELOPED by wil.tor
+
+#ifndef _INFOS_H__
+#define _INFOS_H__
+
+#define WHITE	        "\033[0m"
+#define RED 		"\033[1;31m"
+#define ORANGE          "\033[1;33m"
+#define GREEN  		"\033[1;32m"
+#define BLUE		"\033[1;34m"
+#define CYAN		"\033[1;36m"
+
+#define ERR		RED "[-]" WHITE " "
+#define INFOS		BLUE "[*]" WHITE " "
+#define VALID		GREEN "[+]" WHITE " "
+
+#define VERSION 	"Typhoon: v0.1@linux-x86x64"
+#endif
+
