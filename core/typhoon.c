483ca3a8837b1641e4ab279aaf788792d34145a6
root <root@localhost.localdomain>
2019-01-19 00:08:34 +0100

    in a test period

--- core/typhoon.c
+++ core/typhoon.c
@@ -27,31 +27,13 @@ SOFTWARE.
 #include <stdio.h>
 #include <stdlib.h>
 
-
-/*
- * Header's project file.
- */
-#include "../hexedit/src/hexedit.c"
-
-static char *arguments_list[] =
-{
-	"-H",
-	"--hexedit", // equivalent to '-H'
-
-};
+#include "banner.c"
+#include "errors.h"
 
 int main(int argc, char **argv)
 {
-	if(argc > 1)
-	{
-		printf("tab: %c\n", arguments_list[]);
-		char *buff = save_hexadecimal_format_file((const char*)argv[1]);
-		printf("%s", buff);
-	} else
-	{
-		printf("usage: %s <file_location>.\n", argv[0]);
-		exit(1);
-	}
-
+	printf("\n"
+               INFOS
+               "yo poto");
 	return 0;
 }
